da1e2711ad3c22322be1aa185da8e69f3f4b055b
arnonelucauni01 <114299868+arnonelucauni01@users.noreply.github.com>
2022-09-26 20:51:20 +0200

    Test

--- TRACCIA 5/TRACCIA_5.c
+++ TRACCIA 5/TRACCIA_5.c
@@ -1,13 +1,14 @@
-/*  TRACCIA 4 Sviluppare una function C che,
-* dati come parametri di input un array 2D di double,
-* il numero delle righe e il numero delle colonne,
-* determina e restituisce come para-metro di output il massimo tra le somme degli elementi di ogni colonna.
+/*  TRACCIA 5 Sviluppare una function C che,
+dati come parametri di input un array 2D di int,
+il numero delle righe e il numero delle colonne,
+determina e restituisce come para-metro di output
+il massimo tra le somme degli elementi di ogni riga.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-double sommaColonna(double arr[][5], int col, int rig);
+double sommaRiga(double arr[][5], int col, int rig);
 
 int main()
 {
@@ -29,12 +30,12 @@ int main()
     return 0;
 }
 
-double sommaColonna(double arr[][5], int col, int rig){
+double sommaRiga(double arr[][5], int col, int rig){
     double max = 0;
     double somma = 0;
     for(int i = 0; i < rig; i++){
         for(int j = 0; j < col; j++){
-            somma += arr[j][i];
+            somma += arr[i][j];
         }
         if(max < somma){
             max = somma;
