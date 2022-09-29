33e7c709090777eebd731ad998b047f82464b613
Emanuele <emanuele.pacilio0@gmail.com>
2022-09-29 22:35:18 +0200

    TRACCIA 9
    TRACCIA 7 modificata

--- TRACCIA 7/TRACCIA_7.c
+++ TRACCIA 7/TRACCIA_7.c
@@ -1,16 +1,16 @@
-/*  TRACCIA 7 Scrivere una funzione che dati in input due array
-di strutture del seguente tipo struct prodotto {char *nome; int codice; double prezzo;};
-restituisce in output 1 se i due array di struct sono uguali, 0 se non lo sono
-Si noti che due dati struct sono uguali se sono uguali tutti i loro campi.
+/*  
+    TRACCIA 7 
+        Scrivere una funzione che dati in input due array
+        di strutture del seguente tipo struct prodotto {char *nome; int codice; double prezzo;};
+        restituisce in output 1 se i due array di struct sono uguali, 0 se non lo sono
+        Si noti che due dati struct sono uguali se sono uguali tutti i loro campi.
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 
-const int DIM_P1 = 10;
-const int DIM_P2 = 10;
+#define DIM 10
 
 typedef struct {
     char* nome;
@@ -22,23 +22,24 @@ int confrontaListaProdotti(prodotto p1[], prodotto p2[], int sizeP1, int sizeP2)
 
 int main()
 {
-    prodotto p1[DIM_P1];
-    prodotto p2[DIM_P2];
+    prodotto p1[DIM];
+    prodotto p2[DIM];
 
-    for (int i = 0; i < DIM_P1; i++) {
+    for (int i = 0; i < DIM; i++) {
         p1[i].codice = i;
-        p1[i].prezzo = (i * DIM_P1) / 2;
+        p1[i].prezzo = (i * DIM) / 2;
         p1[i].nome = (char*) malloc(10);
         strcpy(p1[i].nome, "Ciao");
     }
-    for (int i = 0; i < DIM_P2; i++) {
+    
+    for (int i = 0; i < DIM; i++) {
         p2[i].codice = i;
-        p2[i].prezzo = (i * DIM_P1) / 2;
+        p2[i].prezzo = (i * DIM) / 2;
         p2[i].nome = (char*) malloc(10);
         strcpy(p2[i].nome, "Ciao");
     }
 
-    printf("%d", confrontaListaProdotti(p1, p2, DIM_P1, DIM_P2));
+    printf("%d", confrontaListaProdotti(p1, p2, DIM, DIM));
 }
 
 int confrontaListaProdotti(prodotto p1[], prodotto p2[], int sizeP1, int sizeP2) {
