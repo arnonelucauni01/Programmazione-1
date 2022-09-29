f522ccb2923bbc8a72719b1717ef20340d36c56f
Emanuele <emanuele.pacilio0@gmail.com>
2022-09-29 20:15:08 +0200

    TRACCIA 8

---
+++ TRACCIA 8/TRACCIA8.c
@@ -0,0 +1,33 @@
+/*
+    TRACCIA 8
+        Sviluppare una function C che, data come parametro di input una stringa che
+        rappresenta un testo in italiano, determina e restituisce come parametro di output il
+        numero di parole di tre lettere contenute nel testo. Nel testo le parole sono separate
+        da un unico spazio.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void contaParoleTreLettere(char* stringa);
+
+void main(){
+    char stringa[100];
+    printf("inserisci una stringa\n");
+    scanf("%[^\n]%*c", stringa);
+    contaParoleTreLettere(stringa);
+}
+
+void contaParoleTreLettere(char* stringa){
+    int i = 0;
+    char* tmp = strtok(stringa, " ");
+
+    while(tmp != NULL){
+        if(strlen(tmp)==3){
+            i++;
+        }
+        tmp = strtok(NULL, " ");
+    }
+
+    printf("il numero di parole di tre caratteri e`: %d", i);
+}
